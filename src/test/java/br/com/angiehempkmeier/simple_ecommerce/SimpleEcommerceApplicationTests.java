6e128ee2c7193e3c1f2920b85da44ff4227ae989
Angela Hempkmeier <aggsdpz@gmail.com>
2024-06-21 19:52:09 -0300

    feat: initial commit

---
+++ src/test/java/br/com/angiehempkmeier/simple_ecommerce/SimpleEcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.angiehempkmeier.simple_ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleEcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
